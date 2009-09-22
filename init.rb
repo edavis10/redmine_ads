6d47dbe433035498a77cffc5aa8541be66d4239d
Eric Davis <edavis@littlestreamsoftware.com>
2009-09-21 21:29:02 -0700

    Added init information

--- init.rb
+++ init.rb
@@ -4,10 +4,14 @@ require 'redmine'
 require 'redmine_ads/hooks/layout_hooks'
 
 Redmine::Plugin.register :redmine_ads do
-  name 'Redmine Ads plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
+  name 'Redmine Ads'
+  author 'Eric Davis'
+  url 'https://projects.littlestreamsoftware.com/projects/redmine-misc'
+  author_url 'http://www.littlestreamsoftware.com'
+  description 'Redmine ads shows randomized ads in the Redmine sidebar.'
+  version '0.1.0'
+
+  requires_redmine :version_or_higher => '0.8.0'
 
   settings(:partial => 'settings/ads',
            :default => {
